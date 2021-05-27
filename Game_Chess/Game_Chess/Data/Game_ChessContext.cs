7da22894abe7fd90209dcf5028d2ac9e65245739
AkshayChikhalkar <akshaychikhalkar15@gmail.com>
2021-05-27 11:44:37 +0530

    Created Model and View for Board.

---
+++ Game_Chess/Game_Chess/Data/Game_ChessContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Game_Chess.Data
+{
+    public class Game_ChessContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Game_ChessContext() : base("name=Game_ChessContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Game_Chess.Models.Cell> Cells { get; set; }
+
+        public System.Data.Entity.DbSet<Game_Chess.Models.Board> Boards { get; set; }
+    }
+}
---
+++ Game_Chess/Game_Chess/Models/Cell.cs
@@ -0,0 +1,11 @@
+﻿namespace Game_Chess.Models
+{
+    public class Cell
+    {
+        public int Id { get; set; }
+        public int RowNumber { get; set; }
+        public int ColumnNumber { get; set; }
+        public bool CurrentlyOccupied { get; set; }
+        public bool LegalNextMove { get; set; }
+    }
+}
